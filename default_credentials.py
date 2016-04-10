2462cbdfec0184bdf545f8063594802c538ad892
Karthik Hariharan <karthikhariharan13@gmail.com>
2016-04-10 11:47:22 -0500

    Variable length credentials handled

--- default_credentials.py
+++ default_credentials.py
@@ -7,8 +7,11 @@ consumer_key = ["<Enter Consumer Keys here>"]
 consumer_secret = ["<Enter Consumer Secrets here>"]
 
 
+length = len(access_token_key)
+
+
 def get_auth_object(index):
-    i = index % 3
+    i = index % length
     auth = OAuthHandler(consumer_key[i], consumer_secret[i])
     auth.set_access_token(access_token_key[i], access_token_secret[i])
     return auth
